bcba7076b18ee30a58d8773129f85b9c39b954af
Victor Fernandes <victorfdes@users.noreply.github.com>
2020-04-17 23:36:59 +0530

    update menu: keep active menu item open

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 (function () {
+  var activeListItem = document.querySelectorAll('.advanced-menu-plugin .current_page_parent.menu-item-has-children')
+  if (activeListItem) {
+    activeListItem.forEach(function (element) {
+      element.classList.add('open');
+    })
+  }
+  
   var toggles = document.querySelectorAll('.advanced-menu-plugin .advanced-menu-toggle');
   toggles.forEach(function (toggle) {
     toggle.addEventListener('click', function (e) {
